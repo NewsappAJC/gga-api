eb931be301de6ba7ee558e0bf29d4b6f095a7e18
John Perry <jgperry@ajc.com>
2013-12-11 12:14:18 -0500

    finished conversion to json-p

--- app/controllers/api/contributions_sectors_controller.rb
+++ app/controllers/api/contributions_sectors_controller.rb
@@ -2,7 +2,7 @@ module Api
   class ContributionsSectorsController < ApplicationController
     def index
       @sectors = ContributionsSector.where("member_id = #{params[:id]}")
-      render json: @sectors
+      render json: @sectors, callback: params[:callback]
     end
   end
 end
--- app/controllers/api/members_controller.rb
+++ app/controllers/api/members_controller.rb
@@ -2,21 +2,21 @@ module Api
   class MembersController < ApplicationController
     def index
       @members = Member.all
-      render json: @members
+      render json: @members, callback: params[:callback]
     end
 
     def show
       @member = Member.find(params[:id])
-      render json: @member, serializer: MemberDetailSerializer
+      render json: @member, serializer: MemberDetailSerializer, callback: params[:callback]
     end
 
     def member_bills
-      @bills = Member.find(params[:id]).primary_sponsorships
+      @bills = Member.find(params[:id]).primary_sponsorships, callback: params[:callback]
       render json: @bills
     end
 
     def member_committees
-      @committees = Member.find(params[:id]).member_committees
+      @committees = Member.find(params[:id]).member_committees, callback: params[:callback]
       render json: @committees
     end
 
@@ -28,17 +28,17 @@ module Api
 
     def members_by_house
       @members = Member.by_house(params[:house])
-      render json: @members
+      render json: @members, callback: params[:callback]
     end
 
     def members_by_party
       @members = Member.by_party(params[:party])
-      render json: @members
+      render json: @members, callback: params[:callback]
     end
 
     def members_by_house_district
       @members = Member.by_house_district(params[:house], params[:district]).first
-      render json: @members
+      render json: @members, callback: params[:callback]
     end
   end
 end
