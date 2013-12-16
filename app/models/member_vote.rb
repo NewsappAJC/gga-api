d50307a7b5d2bb2ce47923978f4f2c007668a0d1
John Perry <jgperry@ajc.com>
2013-12-16 13:25:39 -0500

    show most recent (10) votes on member page

--- app/models/member_vote.rb
+++ app/models/member_vote.rb
@@ -2,16 +2,17 @@
 #
 # Table name: member_votes
 #
-#  id              :integer          default(0), not null
+#  id              :integer          default(0), not null, primary key
 #  member_id       :integer
 #  vote_id         :integer
 #  voted           :string(10)
 #  vote_code       :integer
-#  bill_id         :integer
+#  vote_date       :datetime
 #  caption         :string(255)
+#  bill_id         :integer
+#  bill_caption    :string(255)
 #  document_type   :string(3)
 #  number          :integer
-#  bill_title      :string(255)
 #  name_first      :string(255)
 #  name_last       :string(255)
 #  name_middle     :string(255)
@@ -27,4 +28,5 @@ class MemberVote < ActiveRecord::Base
   belongs_to :vote
   belongs_to :member
   belongs_to :bill
+  default_scope { order("vote_date desc") }
 end
--- spec/models/member_vote_spec.rb
+++ spec/models/member_vote_spec.rb
@@ -2,16 +2,17 @@
 #
 # Table name: member_votes
 #
-#  id              :integer          default(0), not null
+#  id              :integer          default(0), not null, primary key
 #  member_id       :integer
 #  vote_id         :integer
 #  voted           :string(10)
 #  vote_code       :integer
-#  bill_id         :integer
+#  vote_date       :datetime
 #  caption         :string(255)
+#  bill_id         :integer
+#  bill_caption    :string(255)
 #  document_type   :string(3)
 #  number          :integer
-#  bill_title      :string(255)
 #  name_first      :string(255)
 #  name_last       :string(255)
 #  name_middle     :string(255)
