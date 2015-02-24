7b908c10c699f8a1df274791d933e7096aa77463
John Perry <jgperry@ajc.com>
2015-02-24 16:44:20 -0500

    switch predictions history source from predictions_history to predictions_history_display

--- app/models/predictions_history.rb
+++ app/models/predictions_history.rb
@@ -1,5 +1,5 @@
 class PredictionsHistory < ActiveRecord::Base
-  self.table_name = "predictions_history"
+  self.table_name = "predictions_history_display"
   self.primary_key = "id"
   belongs_to :bill
 end
