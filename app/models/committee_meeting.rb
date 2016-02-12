752b277d1fde3fef4823b3f618adc554e6c081a4
John Perry <jgperry@ajc.com>
2016-02-12 12:12:23 -0500

    Committee meeting model and controller

---
+++ app/models/committee_meeting.rb
@@ -0,0 +1,10 @@
+class CommitteeMeeting < ActiveRecord::Base
+  self.primary_key = 'id'
+
+  scope :today, -> { where("ajc_date = '#{Date.today}'") }
+  scope :tomorrow, -> { where("ajc_date = '#{Date.tomorrow}'") }
+  scope :this_week, -> { where("ajc_date BETWEEN ? AND ?", Date.today.beginning_of_week, Date.today.end_of_week) }
+  scope :next_week, -> { where("ajc_date BETWEEN ? AND ?", Date.today.next_week, Date.today.next_week.end_of_week) }
+  scope :on_date, -> { |date| where("ajc_date = ?", date) }
+  default_scope { order(:ajc_date) }
+end
