fecdabcca2a2c28ce1722bd60ff93bcbc954e0cb
John Perry <jgperry@ajc.com>
2016-02-15 17:31:01 -0500

    While I'm at it, fix the bill even list for the journal page.

--- app/models/committee_meeting.rb
+++ app/models/committee_meeting.rb
@@ -5,6 +5,5 @@ class CommitteeMeeting < ActiveRecord::Base
   scope :tomorrow, -> { where("ajc_date = '#{Date.tomorrow}'") }
   scope :this_week, -> { where("ajc_date BETWEEN ? AND ?", Date.today.beginning_of_week, Date.today.end_of_week) }
   scope :next_week, -> { where("ajc_date BETWEEN ? AND ?", Date.today.next_week, Date.today.next_week.end_of_week) }
-  scope :on_date, -> { |date| where("ajc_date = ?", date) }
   default_scope { order(:ajc_date) }
 end
